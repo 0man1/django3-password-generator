d2970bf18ded4ab7f729f10adc0e27ab5c42868e
0man1 <0man1@mail.ru>
2023-05-04 11:47:14 +0300

    Коммит из PCH

--- password_generator/settings.py
+++ password_generator/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-rsr1@i$zcf1(njcpa1c3ehru*#+f1$)tp2tbm79t#y^l+bbxz)'
+SECRET_KEY = 'django-insecure-rsr1@i$zcf1(njcpa1c3ehru*#+f1$)tp2tbsssm79t#y^l+bbxz)'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
